dd8bd3bc93bab61c1fef913e1858121c99321d21
Alan <60212768+AlanJBX@users.noreply.github.com>
2020-03-26 11:21:28 +0100

    Update script_venv.sh

--- Serverless/script_venv.sh
+++ Serverless/script_venv.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 pip install npm
-mkdir my-flask-application && cd my-flask-application
 npm init -f
 npm install --save-dev serverless-wsgi serverless-python-requirements
 virtualenv venv --python=python3
